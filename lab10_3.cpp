459515074cb1b6d1d0069382df97c6cffd0e4f6c
Jarkrasri Thonglueng <72305051+klyemeo@users.noreply.github.com>
2025-12-23 16:10:35 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,37 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+//cout << "Number of data = ";
+//cout << "Mean = ";
+//cout << "Standard deviation = ";
+
+#include <fstream> 
+#include <iostream>
+#include <iomanip>
+#include <cmath> 
+using namespace std;
+
+int main(){
+    double sum = 0;
+    double i = 0;
+    double dev ; 
+    double othersum; 
+    string textline ; 
+    ifstream source;
+    source.open("score.txt");
+    while (getline(source , textline)){
+        i ++ ;  
+        sum += stod(textline) ; 
+        othersum += pow(stod(textline), 2 ) ; 
+    }
+
+
+
+ 
+    dev = sqrt((othersum/i)-(pow(sum/i, 2)));
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/i << endl;
+    cout << "Standard deviation = " << dev << endl ;
+    //cout << "sum" << sum << endl; 
+}
